140277404760b2046ae159e0f1457d9d48a73195
David Xavier <davidxavier7512@gmail.com>
2025-11-16 22:52:02 +0000

    Init project COMCS

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ 
+ COMCS ALERT SERVER
+ 
+ Requirements:
+    -> UDP Server
+    -> Best QoS in UDP communications
+    -> Ability responde multi clients
+
+ Client UPD request body:
+    -> temperature: { "current": value, "average": value };
+    -> humidity:    { "current": value, "average": value };
+ 
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+        
+    
+    return EXIT_SUCCESS;
+}
